9f84de2ccf75d28e699de28da5401155499d40fb
Francisco Huertas <francisco@fhuertas.com>
2018-03-03 23:49:38 +0100

    Avro functions
    serialize and deserialize functions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,17 @@ import sbt._
 
 
 object Dependencies {
-  val scalaMayorVersion = "2.12"
-  val scalaMinorVersion = s"$scalaMayorVersion.4"
+  lazy val scalaMayorVersion = "2.12"
+  lazy val scalaMinorVersion = s"$scalaMayorVersion.4"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+  lazy val avro4sVersion = "1.8.3"
+  lazy val scalaTestVersion = "3.0.4"
+  lazy val scalaCheckVersion = "1.13.4"
+
+
+  lazy val avro4s = "com.sksamuel.avro4s" %% "avro4s-core" % avro4sVersion // Transitive org.apache.avro 1.8.2
+
+  // Tests
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckVersion
 }
